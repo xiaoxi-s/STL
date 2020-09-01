306c2b6888f11d25fee2258156cb69988f1deaa4
xiaoxi-s <77871407@qq.com>
2020-08-31 22:34:41 -0500

    allocator_base_test added

--- test/allocator_base_test.cpp
+++ test/allocator_base_test.cpp
@@ -2,8 +2,36 @@
 
 #include "../src/sstl_allocator_base.hpp"
 
-TEST(Allocator_base_test, small_try) {
-    sup::allocator_base<int> a;
+// Need to implement: Tests for range construct & destruct
 
-    EXPECT_TRUE(false);
+namespace allocator_base_test {
+
+TEST(Allocator_base_test, allocate_return_null) {
+  sup::allocator_base<int> a;
+  int* ptr = a.allocate(0);
+  EXPECT_TRUE(ptr == nullptr);
 }
+
+class ClassForTest {
+ public:
+  static int object_counter_;  // naming following google c++ style
+  static int destroy_times_;
+  ClassForTest() { ++object_counter_; }
+  ~ClassForTest() { ++destroy_times_; }
+};
+
+int ClassForTest::object_counter_ = 0;
+int ClassForTest::destroy_times_ = 0;
+
+TEST(Allocator_base_test, allocate_not_call_constructor) {
+  sup::allocator_base<ClassForTest> a;
+  ClassForTest* ptr = a.allocate(10);
+  EXPECT_TRUE(ClassForTest::object_counter_ == 0);
+
+  // 10 is a dummy variable
+  a.deallocate(ptr, 10);
+  // deallocate without calling destructors
+  EXPECT_TRUE(ClassForTest::destroy_times_ == 0);
+}
+
+}  // namespace allocator_base_test
