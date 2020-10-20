d09fb97e69576e079c60e332c930fa7c8a673c2e
xiaoxi-s <77871407@qq.com>
2020-10-20 01:03:01 -0500

    pop push_back test added

--- test/list_test.cpp
+++ test/list_test.cpp
@@ -11,10 +11,10 @@ TEST(list_iterator_int_test, test_trait) {
 }
 
 TEST(list_iterator_int_test, test_increment_decrement) {
-  
+  sup::list<int> l;
+  sup::list<int> ::iterator l_it = l.begin();
+  EXPECT_TRUE(++l_it == l.end());
 }
-
-
 }
 
 namespace list_int_test {
@@ -33,8 +33,30 @@ TEST(list_int_test, insert_erase_one_element_test) {
   EXPECT_TRUE(l.size() == 0);
 }
 
-TEST(list_iterator_test, test_increment_decrement) {
-  
+TEST(list_int_test, pop_push_back) {
+  sup::list<int> l;
+  for (int i = 0; i < 10; ++i) {
+    l.push_back(i);
+  }
+
+  sup::list<int>::iterator l_it = l.begin();
+  EXPECT_TRUE(l.size() == 10);
+  for (int i =0; i < 10; ++i) {
+    EXPECT_TRUE(*l_it == i);
+    ++l_it;
+  }
+
+  EXPECT_TRUE(*l.begin() == 0);
+  EXPECT_TRUE(*--l.end() == 9);
+
+  EXPECT_TRUE(l.front() == 0);
+  EXPECT_TRUE(l.back() == 9);
+
+  for (int i = 0; i < 10; ++i) {
+    EXPECT_TRUE(l.back() == 10 - i - 1);
+    l.pop_back();
+  }
+  EXPECT_TRUE(l.size() == 0);
 }
 
 
