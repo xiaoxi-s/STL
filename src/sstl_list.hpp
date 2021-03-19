c0b5073c25a2ac1ae382d38864e85cb75df01a47
adrian <77871407@qq.com>
2021-03-19 00:24:57 -0500

    list & interfaces & tests

--- src/sstl_list.hpp
+++ src/sstl_list.hpp
@@ -1,10 +1,12 @@
 #ifndef _SSTL_LIST_H
 #define _SSTL_LIST_H
 
+#include "type_traits"
 
 #include "sstl_allocator.hpp"
 #include "sstl_construct.hpp"
 #include "sstl_iterator.hpp"
+#include "sstl_type_tratis.hpp"
 
 /**
  * @author Xiaoxi Sun
@@ -23,6 +25,7 @@
  *  - std::allocator_traits used
  *  - std::distance()
  *  - no max_size()
+ *  - std::__is_integer, std::__true_type, and std::__false_type used
  **/
 
 namespace sup {
@@ -140,9 +143,8 @@ class list {
   reference back();
 
   /********* modifiers ********/
-  template <class InputIterator>
-  void assign(InputIterator first, InputIterator last);
-  void assign(size_type n, const value_type& val);
+  template<class InputIterator1, class InputIterator2>
+  void assign(InputIterator1 first, InputIterator2 last);
 
   void push_front(const value_type& val);
   void pop_front();
@@ -197,15 +199,34 @@ class list {
     sup::_construct(&p->data, x);
     return p;
   }
+  // destory data in a node & release the space of the node
   void destory_node(link_type p) {
     sup::_destroy(&p->data);
     put_node(p);
   }
+  // initialize the list when being constructed
   void empty_initialiatize() {
     node = get_node();
     node->next = node;
     node->prev = node;
   }
+  // transfer the content [first, last) to the front of position
+  void transfer(iterator position, iterator first, iterator last) {
+    if (position != last) {
+      (last.node->prev)->next = position.node;
+      (first.node->prev)->next = last.node;
+      (position.node->prev)->next = first.node;
+      link_type tmp = last.node->prev;
+      last.node->prev = first.node->prev;
+      first.node->prev = position.node->prev;
+      position.node->prev =  tmp;
+    }
+  }
+  // for assign
+  template<class InputIterator1, class InputIterator2>
+  void assign_aux(InputIterator1 first, InputIterator2 last, std::__false_type);
+  template<class Size>
+  void assign_aux(Size first, const T& last, std::__true_type);
 
   link_type node;
 };
@@ -213,9 +234,9 @@ class list {
 /**
  * @brief get allocator
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::allocator_type 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::allocator_type - allocator of the type
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::allocator_type list<T, Alloc>::get_allocator() const {
@@ -225,9 +246,9 @@ typename list<T, Alloc>::allocator_type list<T, Alloc>::get_allocator() const {
 /**
  * @brief begin iterator
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::iterator 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::iterator - begin iterator
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::iterator list<T, Alloc>::begin() {
@@ -237,9 +258,9 @@ typename list<T, Alloc>::iterator list<T, Alloc>::begin() {
 /**
  * @brief end iterator
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::iterator 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::iterator - end iterator
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::iterator list<T, Alloc>::end() {
@@ -249,9 +270,9 @@ typename list<T, Alloc>::iterator list<T, Alloc>::end() {
 /**
  * @brief reverse begin iterator
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::reverse_iterator 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::reverse_iterator  - the instance of
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::reverse_iterator list<T, Alloc>::rbegin() {
@@ -259,11 +280,11 @@ typename list<T, Alloc>::reverse_iterator list<T, Alloc>::rbegin() {
 }
 
 /**
- * @brief reverse begin iterator
+ * @brief reverse end iterator
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::reverse_iterator 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::reverse_iterator - the instance of
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::reverse_iterator list<T, Alloc>::rend() {
@@ -275,10 +296,10 @@ typename list<T, Alloc>::reverse_iterator list<T, Alloc>::rend() {
 /**
  * @brief Retur whether this list is empty
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return true 
- * @return false 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return true - the list is empty
+ * @return false - otherwise
  */
 template <class T, class Alloc>
 bool list<T, Alloc>::empty() const {
@@ -288,9 +309,9 @@ bool list<T, Alloc>::empty() const {
 /**
  * @brief return the size of the list
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::size_type 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::size_type - the size
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::size_type list<T, Alloc>::size() const {
@@ -308,9 +329,9 @@ typename list<T, Alloc>::size_type list<T, Alloc>::size() const {
 /**
  * @brief Return the reference to the first element
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::reference 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::reference - the first element
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::reference list<T, Alloc>::front() {
@@ -320,9 +341,9 @@ typename list<T, Alloc>::reference list<T, Alloc>::front() {
 /**
  * @brief return the reference to last element 
  * 
- * @tparam T 
- * @tparam Alloc 
- * @return list<T, Alloc>::reference 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @return list<T, Alloc>::reference - the last element
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::reference list<T, Alloc>::back() {
@@ -330,31 +351,59 @@ typename list<T, Alloc>::reference list<T, Alloc>::back() {
 }
 
 /********* modifiers ********/
-/**
- * @brief create a list of elements specified by range [first, last)
- * 
- * @tparam T 
- * @tparam Alloc 
- * @tparam InputIterator 
- * @param first 
- * @param last 
- */
-template <class T, class Alloc>
-template <class InputIterator>
-void list<T, Alloc>::assign(InputIterator first, InputIterator last) {
 
+// look for below for documents
+template <class T, class Alloc>
+template<class InputIterator1, class InputIterator2>
+void list<T, Alloc>::assign(InputIterator1 first, InputIterator2 last) {
+  typedef typename std::__is_integer<InputIterator1>::__type _Integral;
+
+  assign_aux(first, last, _Integral());
 }
 
 /**
- * @brief create a list of n elements of vals
+ * @brief assign the value based on the values in range [first, last)
  * 
- * @tparam T 
- * @tparam Alloc 
- * @param n 
- * @param val 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @tparam InputIterator1 - iterator type
+ * @tparam InputIterator2 - iterator type
+ * @param first - start of the range
+ * @param last - end of the range
  */
 template <class T, class Alloc>
-void list<T, Alloc>::assign(size_type n, const value_type& val) {}
+template <class InputIterator1, class InputIterator2>
+void list<T, Alloc>::assign_aux(InputIterator1 first, InputIterator2 last, std::__false_type) {
+  if (!this->empty()) {
+    clear();
+  }
+
+  while (first != last) {
+    this->push_back(*first);
+    ++first;
+  }
+}
+
+/**
+ * @brief assign the list to contain n values.
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @tparam Size - size type
+ * @param n - number of values
+ * @param val - the value
+ */
+template <class T, class Alloc>
+template <class Size>
+void list<T, Alloc>::assign_aux(Size n, const T& val, std::__true_type) {
+  if (!this->empty()) {
+    clear();
+  }
+
+  for (size_t i = 0; i < n; ++i) {
+    this->push_back(val);
+  }
+}
 
 /**
  * @brief push an element at the front
@@ -367,6 +416,7 @@ template <class T, class Alloc>
 void list<T, Alloc>::push_front(const value_type& val) {
   insert(begin(), val);
 }
+
 /**
  * @brief pop one element from the front
  * 
@@ -419,41 +469,91 @@ typename list<T, Alloc>::iterator list<T, Alloc>::insert(
   tmp->prev->next = tmp;
   return tmp;
 }
+
+/**
+ * @brief insert a value n times
+ *
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param position - position of insertion
+ * @param val - value of the element
+ */
 template <class T, class Alloc>
 void list<T, Alloc>::insert(iterator position, size_type n,
-                            const value_type& val) {}
+                            const value_type& val) {
+  for(size_type i = 0; i < n; ++i) 
+    insert(position, val);
+}
+
+/**
+ * @brief insert a range of values in the range [first, last)
+ *
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param position - position of insertion
+ * @param val - value of the element
+ */
+
+/**
+ * @brief insert a range of values in the range [first, last)
+ *
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @tparam InputIterator - iterator type 
+ * @param position - after the value to be inserted
+ * @param first - start of the range 
+ * @param last - end of the range
+ */
 template <class T, class Alloc>
 template <class InputIterator>
 void list<T, Alloc>::insert(iterator position, InputIterator first,
-                            InputIterator last) {}
+                            InputIterator last) {
+  while(first != last) 
+    insert(position, *first);
+}
 
 /**
  * @brief remove and destory the element pointed by position
  * 
- * @tparam T 
- * @tparam Alloc 
- * @param position 
- * @return list<T, Alloc>::iterator 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param position - position of insertion
+ * @return list<T, Alloc>::iterator - an iterator to the element after the removed element
  */
 template <class T, class Alloc>
 typename list<T, Alloc>::iterator list<T, Alloc>::erase(iterator position) {
+  link_type next = position.node->next;
   link_type curr = position.node;
   curr->prev->next = curr->next;
   curr->next->prev = curr->prev;
   destory_node(curr);
 
-  return curr;
+  return iterator(next);
 }
 
+/**
+ * @brief remove elements in the range [first, last)
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param first - start position 
+ * @param last - end position
+ * @return list<T, Alloc>::iterator 
+ */
 template <class T, class Alloc>
 typename list<T, Alloc>::iterator list<T, Alloc>::erase(iterator first,
-                                                        iterator last) {}
+                                                        iterator last) {
+  while(first != last) {
+    first = erase(first);
+  }
+  return last;
+}
 
 /**
  * @brief swap the content of two lists
  * 
- * @tparam T 
- * @tparam Alloc 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
  * @param x - another list
  */
 template <class T, class Alloc>
@@ -463,14 +563,28 @@ void list<T, Alloc>::swap(list& x) {
   this->node = temp;
 }
 
+/**
+ * @brief clear the list and fill in with n vals
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param n - the number of vals
+ * @param val - the value
+ */
 template <class T, class Alloc>
-void list<T, Alloc>::resize(size_type n, value_type val) {}
+void list<T, Alloc>::resize(size_type n, value_type val) {
+  if (!empty()) 
+    clear();
+
+  for (size_type i = 0; i < n; ++i) 
+    push_back(val);
+}
 
 /**
  * @brief clear the list
  * 
- * @tparam T 
- * @tparam Alloc 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
  */
 template <class T, class Alloc>
 void list<T, Alloc>::clear() {
@@ -485,20 +599,62 @@ void list<T, Alloc>::clear() {
 }
 
 /******** Operations ********/
+
+/**
+ * @brief transfer the list x into the place before position in *this
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param position - the place before which x would be inserted
+ * @param x - the input list
+ */
 template <class T, class Alloc>
-void list<T, Alloc>::splice(iterator position, list& x) {}
+void list<T, Alloc>::splice(iterator position, list& x) {
+  if (!x.empty()) 
+    transfer(position, x.begin(), x.end());
+}
+
+/**
+ * @brief transfer a single element into the place before position in *this
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param position - the place before which the element would be inserted
+ * @param x - dummy variable
+ * @param i - transfer a single element pointed by iterator i
+ */
 template <class T, class Alloc>
-void list<T, Alloc>::splice(iterator position, list& x, iterator i) {}
+void list<T, Alloc>::splice(iterator position, list&, iterator i) {
+  iterator j = i;
+  ++j;
+  if (position == i || position == j) return; 
+  transfer(position, i, j);
+}
+
+/**
+ * @brief transfer a range [first, last) of elements into the place before position in *this
+ *  position should not be in the range of [first, last)
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param position - the place before which the element would be inserted
+ * @param first - the start element 
+ * @param last - the end element
+ */
 template <class T, class Alloc>
-void list<T, Alloc>::splice(iterator position, list& x, iterator first,
-                            iterator last) {}
+void list<T, Alloc>::splice(iterator position, list&, iterator first,
+                            iterator last) {
+  if (first != last) {
+    transfer(position, first, last);
+  }
+}
 
 /**
  * @brief remove all vals
  * 
- * @tparam T 
- * @tparam Alloc 
- * @param val 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param val - the values to be removed
  */
 template <class T, class Alloc>
 void list<T, Alloc>::remove(const value_type& val) {
@@ -512,6 +668,14 @@ void list<T, Alloc>::remove(const value_type& val) {
     }
   }
 }
+/**
+ * @brief remove according to a predicate
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @tparam Predicate - predicate type
+ * @param pred - the predicate applied to value
+ */
 template <class T, class Alloc>
 template <class Predicate>
 void list<T, Alloc>::remove_if(Predicate pred) {
@@ -527,37 +691,205 @@ void list<T, Alloc>::remove_if(Predicate pred) {
 }
 
 /**
- * @brief remove consecutive values
+ * @brief remove consecutive same values
  * 
- * @tparam T 
- * @tparam Alloc 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
  */
 template <class T, class Alloc>
-void list<T, Alloc>::unique() {}
+void list<T, Alloc>::unique() {
+  if (node->next == node || node->next->next == node) return;
 
+  iterator first = begin();
+  iterator second = first;
+  ++second;
+
+  while(second != end()) {
+    if (*first == *second) {
+      second = erase(second);
+    } else {
+      ++first;
+      ++second;
+    }
+  }
+}
+
+/**
+ * @brief remove consecutive same values according to a predicate
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @tparam BinaryPredicate - a predicate
+ */
 template <class T, class Alloc>
 template <class BinaryPredicate>
-void list<T, Alloc>::unique(BinaryPredicate binary_pred) {}
+void list<T, Alloc>::unique(BinaryPredicate binary_pred) {
+    if (node->next == node || node->next->next == node) return;
 
+  iterator first = begin();
+  iterator second = first;
+  ++second;
+
+  while(second != end()) {
+    if (binary_pred(*first, *second)) {
+      second = erase(second);
+    } else {
+      ++first;
+      ++second;
+    }
+  }
+}
+
+/**
+ * @brief merge this list and the input list assuming they are in an increasing order
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @param x the sorted list to be merged
+ */
 template <class T, class Alloc>
-void list<T, Alloc>::merge(list& x) {}
+void list<T, Alloc>::merge(list& x) {
+  iterator first1 = this->begin();
+  iterator last1 = this->end();
+  iterator first2 = x.begin();
+  iterator last2 = x.end();
 
+  while(first1 != last1 && first2 != last2) {
+    if (*first1 < *first2) {
+      ++first1;
+    } else {
+      iterator next = first2;
+      transfer(first1, first2, ++next);
+      first2 = next;
+    }
+  }
+
+  if (first2 != last2) {
+    transfer(last1, first2, last2);
+  }
+}
+
+/**
+ * @brief merge with comparator
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @tparam Compare - function type
+ * @param x - the listed to be merged with *this
+ * @param comp - the comparator function
+ */
 template <class T, class Alloc>
 template <class Compare>
-void list<T, Alloc>::merge(list& x, Compare comp) {}
+void list<T, Alloc>::merge(list& x, Compare comp) {
+  iterator first1 = this->begin();
+  iterator last1 = this->end();
+  iterator first2 = x.begin();
+  iterator last2 = x.end();
 
+  while(first1 != last1 && first2 != last2) {
+    if (comp(*first1, *first2)) {
+      ++first1;
+    } else {
+      iterator next = first2;
+      transfer(first1, first2, ++next);
+      first2 = next;
+    }
+  }
+
+  if (first2 != last2) {
+    transfer(last1, first2, last2);
+  }
+}
+
+/**
+ * @brief sort the list in an infreasing order
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ */
 template <class T, class Alloc>
-void list<T, Alloc>::sort() {}
+void list<T, Alloc>::sort() {
+  if (node->next == node || node->next->next == node) return;
 
+  // itermediate lists
+  list<T, Alloc> carry;
+  list<T, Alloc> counter[64];
+
+  int fill = 0;
+
+  while(!empty()) {
+    carry.splice(carry.begin(), *this, begin());
+    int i = 0;
+    while( i < fill && !counter[i].empty()) {
+      counter[i].merge(carry);
+      carry.swap(counter[i++]);
+    }
+    carry.swap(counter[i]);
+    if (i == fill) ++fill;
+  }
+
+  for (int i = 1; i < fill; ++i) 
+    counter[i].merge(counter[i-1]);
+
+  swap(counter[fill-1]);
+}
+
+/**
+ * @brief sort with a comparator
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ * @tparam Compare - function type
+ * @param comp - the comparator function
+ */
 template <class T, class Alloc>
 template <class Compare>
-void list<T, Alloc>::sort(Compare comp) {}
+void list<T, Alloc>::sort(Compare comp) {
+  if (node->next == node || node->next->next == node) return;
 
-template <class T, class Alloc>
-void list<T, Alloc>::reverse() {}
+  // itermediate lists
+  list<T, Alloc> carry;
+  list<T, Alloc> counter[64];
 
+  int fill = 0;
+
+  while(!empty()) {
+    carry.splice(carry.begin(), *this, begin());
+    int i = 0;
+    while( i < fill && !counter[i].empty()) {
+      counter[i].merge(carry, comp);
+      carry.swap(counter[i++]);
+    }
+    carry.swap(counter[i]);
+    if (i == fill) ++fill;
+  }
+
+  for (int i = 1; i < fill; ++i) 
+    counter[i].merge(counter[i-1], comp);
+    
+  swap(counter[fill-1]);
+}
+
+/**
+ * @brief reverse the list
+ * 
+ * @tparam T - element type parameter
+ * @tparam Alloc - allocator type
+ */
 template <class T, class Alloc>
-typename list<T, Alloc>::allocator_type get_allocator() {}
+void list<T, Alloc>::reverse() {
+  if (node->next == node || node->next->next == node) return;
+
+  iterator first = begin();
+  ++first;
+  
+  while(first != end()) {
+    iterator old = first;
+    ++first;
+    transfer(begin(), old, first);
+  }
+}
+
 }  // namespace sup
 
 #endif
--- test/list_test.cpp
+++ test/list_test.cpp
@@ -33,6 +33,33 @@ TEST(list_int_test, insert_erase_one_element_test) {
   EXPECT_TRUE(l.size() == 0);
 }
 
+
+TEST(list_int_test, erase_range) {
+  sup::list<int> l;
+  for (int i = 0; i < 10; ++i) {
+    l.push_back(i);
+  }
+
+  sup::list<int>::iterator first = l.begin();
+  sup::list<int>::iterator last = l.begin();
+
+  for (int i = 0; i < 3; ++i) ++first;
+  for (int i = 0; i < 6; ++i)  ++last;
+
+  last = l.erase(first, last);
+
+  EXPECT_TRUE(l.size() == 7);
+  first = l.begin();
+
+  for (int i = 0; first != last; ++first, ++i) {
+    EXPECT_TRUE(i == *first);
+  }
+
+  for (int i = 6; first != l.end(); ++first, ++i) {
+    EXPECT_TRUE(i == *first);
+  }
+}
+
 TEST(list_int_test, pop_push_back) {
   sup::list<int> l;
   for (int i = 0; i < 10; ++i) {
@@ -132,4 +159,236 @@ TEST(list_int_test, swap_test) {
   }
 }
 
+TEST(list_int_test, splice_list_test) {
+  sup::list<int> l1;
+  sup::list<int> l2;
+  for (int i = 0; i < 10; ++i) 
+    l1.push_back(i);
+
+  for (int i = 20; i < 25; ++i)
+    l2.push_back(i);
+
+  l1.splice(l1.begin(), l2);
+  EXPECT_TRUE(l2.empty());
+
+  sup::list<int>::iterator it = l1.begin();
+  
+  // list "l2"
+  for (int i = 20; i < 25; ++i) {
+    EXPECT_TRUE(*it == i);
+    ++it; 
+  }
+  
+  // list "l1"
+  for (int i = 0; i < 10; ++i) {
+    EXPECT_TRUE(*it == i);
+    ++it;
+  }
+}
+
+TEST(list_int_test, splice_single_element_test) {
+  sup::list<int> l1;
+  sup::list<int> l2;
+  for (int i = 0; i < 10; ++i) 
+    l1.push_back(i);
+
+  for (int i = 20; i < 25; ++i)
+    l2.push_back(i);
+
+  sup::list<int>::iterator it2 = l2.begin();
+  l1.splice(l1.begin(), l2, it2);
+  EXPECT_TRUE(l2.size() == 4);
+
+  sup::list<int>::iterator it1 = l1.begin();
+  EXPECT_TRUE(*it1 == 20);
+  ++it1;
+  for (int i = 0;it1 != l1.end(); ++it1, ++i) {
+    EXPECT_TRUE(*it1 == i);
+  }
+}
+
+TEST(list_int_test, splice_range_test) {
+  sup::list<int> l1;
+  sup::list<int> l2;
+  for (int i = 0; i < 10; ++i) 
+    l1.push_back(i);
+
+  for (int i = 20; i < 25; ++i)
+    l2.push_back(i);
+
+  sup::list<int>::iterator it2 = l2.begin();
+  ++it2;
+  ++it2; 
+
+  l1.splice(l1.begin(), l2, l2.begin(), it2);
+  EXPECT_TRUE(l2.size() == 3);
+  sup::list<int>::iterator it1 = l1.begin();
+  
+  for (int i = 20; i < 22; ++it1, ++i) {
+    EXPECT_TRUE(*it1 == i);
+  }
+  for (int i = 0; i < 10; ++it1, ++i) {
+    EXPECT_TRUE(*it1 == i);
+  }
+}
+
+TEST(list_int_test, merge) {
+  sup::list<int> l1;
+  sup::list<int> l2;
+  for (int i = 0; i < 10; i += 2) 
+    l1.push_back(i);
+  for (int i = 1; i < 10; i += 2)
+    l2.push_back(i);
+
+  l1.merge(l2);
+  EXPECT_TRUE(l2.empty());
+
+  sup::list<int>::iterator it = l1.begin();
+  for (int i = 0; i < 10; ++it, ++i) {
+    EXPECT_TRUE(*it == i);
+  }
+}
+
+TEST(list_int_test, merge_with_comparator) {
+  sup::list<int> l1;
+  sup::list<int> l2;
+
+  // create two decreasing array
+  for (int i = 9; i >= 0; --i) 
+    l1.push_back(i);
+  for (int i = 24; i >= 20; --i)
+    l2.push_back(i);
+
+  // merge two lists in an decreasing order
+  l1.merge(l2, [](int a, int b) {
+            return (a > b);});
+  EXPECT_TRUE(l2.empty());
+
+  sup::list<int>::iterator it = l1.begin();
+  for(int i = 24; i >= 20; ++it, --i) {
+    printf("%d %d",*it, i);
+    EXPECT_TRUE(*it == i);
+  }
+
+  for(int i = 9; i >= 0; ++it, --i) {
+    EXPECT_TRUE(*it == i);
+  }
+}
+
+TEST(list_int_test, reverse) {
+  sup::list<int> l1;
+
+  for (int i = 0; i < 10; ++i) 
+    l1.push_back(i);
+
+  l1.reverse();
+
+  sup::list<int>::iterator it = l1.begin();
+  for(int i = 9; it != l1.end(); ++it, --i) {
+    EXPECT_TRUE(*it == i);
+  }
+}
+
+
+TEST(list_int_test, assign_with_range) {
+  sup::list<int> l1;
+  sup::list<int> l2;
+
+  for (int i = 0; i < 10; ++i) 
+    l1.push_back(i);
+  for (int i = 20; i < 25; ++i)
+    l2.push_back(i);
+
+  l1.assign(l2.begin(), l2.end());
+  
+  sup::list<int>::iterator it2 = l2.begin();
+  sup::list<int>::iterator it1 = l1.begin();
+  
+  // l2 does not change
+  // and l1 contains the same sequence (not same nodes) as l2
+  for (int i = 20; it2 != l2.end(); ++it2, ++it1, ++i) {
+    EXPECT_TRUE(*it2 == i);
+    EXPECT_TRUE(*it1 == i);
+  }
+}
+
+TEST(list_int_test, assign_with_values) {
+  sup::list<int> l1;
+
+  for (int i = 0; i < 10; ++i) 
+    l1.push_back(i);
+
+  l1.assign(5, 999);
+
+  sup::list<int>::iterator it1 = l1.begin();
+
+  EXPECT_TRUE(l1.size() == 5);
+  for (;it1 != l1.end(); ++it1) {
+    EXPECT_TRUE(*it1 == 999);
+  }
+}
+
+TEST(list_int_test, unique_simple) {
+  sup::list<int> l1;
+
+  l1.assign(5, 999);
+  l1.unique();
+  EXPECT_TRUE(l1.size() == 1);
+  EXPECT_TRUE(*l1.begin() == 999);
+}
+
+TEST(list_int_test, unique_predicate) {
+  sup::list<int> l1;
+  for (int i = 0; i < 10; ++i) 
+    l1.push_back(i);
+
+
+  // false unique!!!
+  l1.unique([](int a, int b) {
+            return (a != b);});
+  EXPECT_TRUE(l1.size() == 1);
+}
+
+TEST(list_int_test, sort_simple) {
+  sup::list<int> l1;
+  for (int i = 9; i >= 0; --i) 
+    l1.push_back(i);
+
+  l1.sort();
+
+  sup::list<int>::iterator it1 = l1.begin();
+  for (int i = 0; i < 10; ++it1, ++i) {
+    EXPECT_TRUE(*it1 == i);
+  }
+}
+
+TEST(list_int_test, sort_predicate) {
+  sup::list<int> l1;
+  for (int i = 0; i<10; ++i) 
+    l1.push_back(i);
+
+  // sort in an decreasing order
+  l1.sort([](int a, int b) {
+            return (a > b);});
+
+  sup::list<int>::iterator it1 = l1.begin();
+  for (int i = 9; i >= 0; ++it1, --i)  {
+    EXPECT_TRUE(*it1 == i);
+  }
+}
+
+TEST(list_int_test, resize) {
+  sup::list<int> l1;
+  for (int i = 0; i<10; ++i) 
+    l1.push_back(i);
+
+  l1.resize(3, 999);
+
+  EXPECT_TRUE(l1.size() == 3);
+
+  sup::list<int>::iterator it1 = l1.begin();
+  for (int i = 0; it1 != l1.end(); ++it1, ++i) {
+    EXPECT_TRUE(*it1 == 999);
+  }
+}
 }
