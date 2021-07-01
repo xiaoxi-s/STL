98f4830f63ed165b6a7862cf98699b8c34d57e1c
xiaoxi-s <77871407@qq.com>
2021-07-01 16:22:06 +0800

    algorithm header update

--- src/sstl_algorithm.hpp
+++ src/sstl_algorithm.hpp
@@ -1,6 +1,9 @@
 #ifndef _SSTL_ALGORITHM_H
 #define _SSTL_ALGORITHM_H
 
+#include "./algorithms/sstl_binary_operations.hpp"
 #include "./algorithms/sstl_heap.hpp"
+#include "./algorithms/sstl_set_operations.hpp"
+#include "./algorithms/sstl_sort.hpp"
 
 #endif
